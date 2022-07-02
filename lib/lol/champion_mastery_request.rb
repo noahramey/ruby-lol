967b133e07f2afe0d97cc6141722b303febdfa38
Noah <noahramey1997@gmail.com>
2022-07-02 14:59:43 -0700

    clean up champion mastery request

--- lib/lol/champion_mastery_request.rb
+++ lib/lol/champion_mastery_request.rb
@@ -11,29 +11,30 @@ module Lol
     # Get a player's total champion mastery score, which is the sum of individual champion mastery levels
     #
     # See: https://developer.riotgames.com/api-methods/#champion-mastery-v3/GET_getDynamicModelScore
-    # @param [Integer] encrypted_summoner_id Encrypted Summoner ID associated with the player
+    # @param [String] summoner_id Summoner ID associated with the player
     # @return [Integer] Player's total champion master score
-    def total_score encrypted_summoner_id:
-      perform_request api_url "scores/by-summoner/#{encrypted_summoner_id}"
+    def total_score(summoner_id)
+      perform_request api_url "scores/by-summoner/#{summoner_id}"
     end
 
     # Get all champion mastery entries sorted by number of champion points descending
     #
     # See: https://developer.riotgames.com/api-methods/#champion-mastery-v4/GET_getAllChampionMasteries
-    # @param [Integer] encrypted_summoner_id Encrypted Summoner ID associated with the player
+    # @param [Integer] String Summoner ID associated with the player
     # @return [Array<Lol::DynamicModel>] Champion Masteries
-    def all encrypted_summoner_id:
-      result = perform_request api_url "champion-masteries/by-summoner/#{encrypted_summoner_id}"
+    def all(summoner_id)
+      result = perform_request api_url "champion-masteries/by-summoner/#{summoner_id}"
       result.map { |c| DynamicModel.new c }
     end
 
     # Get a champion mastery by player ID and champion ID
     #
     # See: https://developer.riotgames.com/api-methods/#champion-mastery-v4/GET_getDynamicModel
-    # @param [Integer] encrypted_summoner_id Encrypted Summoner ID associated with the player
+    # @param [String] summoner_id Summoner ID associated with the player
+    # @param [Integer] champion_id Champion ID
     # @return [Lol::DynamicModel] Champion Mastery
-    def find champion_id, encrypted_summoner_id:
-      result = perform_request api_url "champion-masteries/by-summoner/#{encrypted_summoner_id}/by-champion/#{champion_id}"
+    def find(summoner_id, champion_id)
+      result = perform_request api_url "champion-masteries/by-summoner/#{summoner_id}/by-champion/#{champion_id}"
       DynamicModel.new result
     end
   end
